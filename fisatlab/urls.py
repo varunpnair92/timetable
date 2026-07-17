a60bb4ff09c8037dd7e9178e35555a9ce75e3bd9
varunpnair92 <varun@Varuns-MacBook-Pro-2.local>
2026-07-17 23:46:38 +0530

    Allow media file serving under all environments and add direct Open in New Tab preview button fallback

--- fisatlab/urls.py
+++ fisatlab/urls.py
@@ -24,6 +24,10 @@ urlpatterns = [
     path('', include('fisat.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.views.static import serve
+from django.urls import re_path
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
 
